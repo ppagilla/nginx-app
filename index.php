2213140c7e28088796bf6363dda40f7ded0a7bae
ppagilla <137497963+ppagilla@users.noreply.github.com>
2024-01-11 13:24:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<title> RM AWS ELB/ASG DEMO </title>
+<body>
+<h1>
+<center>
+<IMG SRC="omg.jpg" ALT="omg"><br>
+<?php 
+echo $_SERVER['SERVER_ADDR'];
+?>
+</center>
+</h1>
+</body>
+</html>
